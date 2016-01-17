f14d083e6f408ebc5010472f5960147cae56c380
Darwin Salgado <darwindeveloping@gmail.com>
2016-01-16 22:26:43 -0500

    adding continuation

--- index.php
+++ index.php
@@ -129,51 +129,6 @@ IMG{
     </div>
     <div class="ui-body ui-body-a" data-form="ui-body-a" data-theme="a"  id="places" role="main">
     </div>
-    <!--
-    <ul data-inset="true" data-role="listview" class="ui-listview ui-listview-inset ui-corner-all ui-shadow">
-        <li data-form="ui-bar-a" data-swatch="a" data-theme="a" data-role="list-divider" role="heading" class="ui-li-divider ui-bar-a ui-first-child">List Header</li>
-        <li data-theme="a" data-swatch="a" data-form="ui-body-a" class="ui-li-static ui-body-a">Read-only list item</li>
-        <li class="ui-last-child"><a href="#" data-theme="a" data-swatch="a" data-form="ui-btn-up-a" class="ui-btn-a ui-btn ui-btn-icon-right ui-icon-carat-r">Linked list item</a></li>
-    </ul>
-
-    <div data-role="fieldcontain" class="ui-field-contain">
-        <fieldset data-role="controlgroup" class="ui-controlgroup ui-controlgroup-vertical ui-corner-all"><div class="ui-controlgroup-controls ">
-                <div class="ui-radio"><label data-form="ui-btn-up-a" for="radio-choice-1-a" class="ui-btn ui-corner-all ui-btn-a ui-btn-icon-left ui-radio-on ui-first-child">Radio</label><input type="radio" checked="checked" value="choice-1" id="radio-choice-1-a" name="radio-choice-a" data-theme="a"></div>
-
-
-                <div class="ui-checkbox"><label data-form="ui-btn-up-a" for="checkbox-a" class="ui-btn ui-corner-all ui-btn-a ui-btn-icon-left ui-checkbox-on ui-last-child">Checkbox</label><input type="checkbox" checked="checked" id="checkbox-a" name="checkbox-a" data-theme="a"></div>
-
-
-            </div></fieldset>
-    </div>
-
-    <div data-role="fieldcontain" class="ui-field-contain">
-        <fieldset data-type="horizontal" data-role="controlgroup" class="ui-controlgroup ui-controlgroup-horizontal ui-corner-all"><div class="ui-controlgroup-controls ">
-                <div class="ui-radio"><label data-form="ui-btn-up-a" for="radio-view-a-a" class="ui-btn ui-corner-all ui-btn-a ui-radio-on ui-btn-active ui-first-child">On</label><input type="radio" checked="checked" value="list" id="radio-view-a-a" name="radio-view-a" data-theme="a"></div>
-
-                <div class="ui-radio"><label data-form="ui-btn-up-a" for="radio-view-b-a" class="ui-btn ui-corner-all ui-btn-a ui-radio-off ui-last-child">Off</label><input type="radio" value="grid" id="radio-view-b-a" name="radio-view-a" data-theme="a"></div>
-
-            </div></fieldset>
-    </div>
-
-    <div data-role="fieldcontain" class="ui-field-contain">
-        <div class="ui-select"><a href="#" role="button" id="select-choice-a-button" aria-haspopup="true" class="ui-btn ui-icon-carat-d ui-btn-icon-right ui-btn-a ui-corner-all ui-shadow" data-theme="a" data-form="ui-btn-up-a"><span>Option 1</span></a><select data-form="ui-btn-up-a" data-theme="a" data-native-menu="false" id="select-choice-a" name="select-choice" tabindex="-1">
-                <option value="standard">Option 1</option>
-                <option value="rush">Option 2</option>
-                <option value="express">Option 3</option>
-                <option value="overnight">Option 4</option>
-            </select><div style="display: none;" id="select-choice-a-listbox-placeholder"></div></div>
-    </div>
-
-    <div class="ui-input-text ui-body-a ui-corner-all ui-shadow-inset"><input type="text" data-form="ui-body-a" class="input" value="Text Input" data-theme="a"></div>
-
-    <div data-role="fieldcontain" class="ui-field-contain">
-        <div class="ui-slider"><input type="number" data-type="range" data-highlight="true" data-theme="a" data-form="ui-body-a" max="100" min="0" value="50" name="slider" class="ui-shadow-inset ui-body-a ui-corner-all ui-slider-input"><div role="application" class="ui-slider-track ui-shadow-inset ui-bar-a ui-corner-all ui-btn-up-a" data-form="ui-btn-up-a" data-theme="a"><div class="ui-slider-bg ui-btn-active" style="width: 50%;"></div><a href="#" class="ui-slider-handle ui-btn ui-btn-a ui-shadow" role="slider" aria-valuemin="0" aria-valuemax="100" aria-valuenow="50" aria-valuetext="50" title="50" aria-labelledby="undefined-label" style="left: 50%;" data-form="ui-btn-up-a" data-theme="a"></a></div></div>
-    </div>
-
-    <button data-form="ui-btn-up-a" data-theme="a" data-icon="star" class=" ui-btn ui-btn-a ui-icon-star ui-btn-icon-left ui-shadow ui-corner-all">Button</button>
-</div>
--->
   <div data-role="footer"  data-theme="a" data-form="ui-page-theme-a" class="ui-content ui-page-theme-a">
     <h1>&copy; 2016 My Places</h1>
   </div>
